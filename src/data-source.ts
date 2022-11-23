6b900021294a3c5c6fb2008677241fcc714f79e1
Loadeksdi <nicolas.load@gmail.com>
2022-11-23 16:01:17 +0100

    Dev: 2 tests done

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,5 @@
 import { DataSource } from 'typeorm';
-import { User } from './entity/user';
+import { User } from './entities/user';
 import * as dotenv from 'dotenv';
 dotenv.config()
 
--- src/specs/entities/user.spec.ts
+++ src/specs/entities/user.spec.ts
@@ -1,8 +1,8 @@
 import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
 import { AppDataSource } from '../../data-source'
-import { DataSource } from 'typeorm'
-import { User } from '../../entity/User'
+import { DataSource, QueryFailedError } from 'typeorm'
+import { User } from '../../entities/user'
 
 chai.use(chaiAsPromised)
 
@@ -25,11 +25,14 @@ describe('User', () => {
             user.passwordHash = 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86';
             await datasource.getRepository(User).save(user);
             const userInDB = await datasource.getRepository(User).findOne({ where: { firstname: user.firstname } });
-            chai.expect(userInDB).eql(user);
+            chai.expect(userInDB).deep.equal(user);
         });
         it('should raise error if email is missing', async function () {
-            // hint to check if a promise fails with chai + chai-as-promise:
-            // await chai.expect(promise).to.eventually.be.rejectedWith(QueryFailedError, "message")
+            const user = new User();
+            user.firstname = 'John';
+            user.lastname = 'Doe';
+            user.passwordHash = 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86';
+            await chai.expect(datasource.getRepository(User).save(user)).to.eventually.be.rejectedWith(QueryFailedError);
         })
     })
 })
